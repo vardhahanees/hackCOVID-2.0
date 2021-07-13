bf0ec4b46462389c11bf4a5b66f13a37fce4df03
Vardhah Anees <63852903+vardhahanees@users.noreply.github.com>
2021-07-13 22:28:06 +0400

    Add files via upload

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '75dfe9e31ec8148edb841d94edf17970'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '97ea564a-cbfa-40b5-bd7c-335fd7e54c85'
+};
